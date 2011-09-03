f9f49d3a026456629f7ac804d842921d07aa07ec
Dan Connolly <http://www.madmode.com/>
2011-09-03 16:13:13 -0500

    generated by xataface makesite

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * File: index.php
+ * Description:
+ * -------------
+ *
+ * This is an entry file for this Dataface Application.  To use your application
+ * simply point your web browser to this file.
+ */
+$time = microtime(true);
+	// use the timer to time how long it takes to generate a page
+require_once '/usr/local/src/xataface-1.3rc6/dataface-public-api.php';
+	// include the initialization file
+df_init(__FILE__, 'http://pav/xataface-1.3rc6');
+	// initialize the site
+
+$app =& Dataface_Application::getInstance();
+	// get an application instance and perform initialization
+$app->display();
+	// display the application
+
+
+$time = microtime(true) - $time;
+echo "<p>Execution Time: $time</p>";
+?>
