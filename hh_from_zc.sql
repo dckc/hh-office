2290c0b9c567201b7b0cf4125a63ea7d27aced68
Dan Connolly <http://www.madmode.com/>
2011-09-11 20:52:45 -0500

    database tweaks: Therapist, Officer can be null; etc.

--- hh_from_zc.sql
+++ hh_from_zc.sql
@@ -4,19 +4,22 @@ use hh_office;
 truncate table Therapist;
 truncate table "Group";
 truncate table "Session";
+truncate table "Officer";
 truncate table "Client";
 truncate table "Visit";
 
 insert into Therapist (id, name)
 select null, name
-from zc.Therapist;
+from zc.Therapist
+order by name;
 
 insert into "Group" (
   id, name, rate, evaluation,
   id_zoho, id_dabble )
 select null, name, rate, evaluation
      , id_zoho, id_dabble
-from zc."Group";
+from zc."Group"
+order by name;
 
 insert into "Session" (
  id, session_date, Therapist_id, time, Group_id,
@@ -31,17 +34,19 @@ select null
      , s.id_dabble
 from zc.Session s
 join "Group" g on g.id_zoho = s.Group_id_zoho
-left join Therapist t on t.name = s.Therapist_name;
+left join Therapist t on t.name = s.Therapist_name
+order by session_date, g.name;
 
 insert into Officer (
  name, email, id_zoho, id_dabble
 )
 select name, email, id_zoho, id_dabble
-from zc.Officer;
+from zc.Officer
+order by name;
 
 insert into Client (
        name, insurance, approval, DX, note
-     , address, phone, DOB, file, file_site, file_opened, ho.Officer_id
+     , address, phone, DOB, file, file_site, file_opened, Officer_id
      , id_zoho, id_dabble
 )
 select
@@ -49,7 +54,8 @@ select
      , address, phone, DOB, file, file_site, file_opened, ho.id as Officer_id
      , zc.id_zoho, zc.id_dabble
 from zc.Client zc
-left join Officer ho on ho.id_zoho = zc.Officer_id_zoho;
+left join Officer ho on ho.id_zoho = zc.Officer_id_zoho
+order by name;
 
 create index client_id_zoho on Client(id_zoho);
 create index session_id_zoho on Session(id_zoho);
@@ -79,4 +85,6 @@ select s.id as Session_id
      , v.id_zoho as id_zoho, v.id_dabble
 from zc.Visit v
 join Client c on v.Client_id_zoho = c.id_zoho
-join `Session` s on v.Session_id_zoho = s.id_zoho;
+join `Session` s on v.Session_id_zoho = s.id_zoho
+order by s.session_date, c.name;
+
