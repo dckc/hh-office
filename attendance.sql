99997c1af528e05da7236ae3032dd4adcd1c0a35
Dan Connolly <http://www.madmode.com/>
2011-09-16 21:19:49 -0500

    include officer in Attendance_by_Group view

--- attendance.sql
+++ attendance.sql
@@ -1,15 +1,25 @@
+use hh_office;
+
 create or replace view Attendance_by_Group as
 select g.id as group_id, g.name as group_name
-     , c.id as client_id, c.name as client_name
+     , c.id as client_id, c.name as client_name, o.name as officer_name
      , date_format(s.session_date, '%Y-%m-%d') as session_date
      , attend_n, charge, client_paid
      , insurance_paid, due
      , v.note
-from zvisit as v
-join zsession as s
+from Visit as v
+join `Session` as s
   on v.session_id = s.id
-join zgroup as g
+join `Group` as g
   on s.group_id = g.id
-join zclient c
+join Client c
   on v.client_id = c.id
-order by g.name, c.name, s.session_date
+left join Officer o on c.officer_id = o.id;
+-- order by g.name, c.name, s.session_date;
+select * from hh_office.Attendance_by_Group
+order by group_name, client_name, session_date;
+
+select c.name, o.name
+from Client c
+left join Officer o on c.officer_id = o.id
+order by c.name;
