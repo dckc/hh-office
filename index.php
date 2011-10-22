15f89e9c14bf16e139af437b8ed9a8e1ed0ea8ad
Dan Connolly <http://www.madmode.com/>
2011-10-22 11:01:41 -0700

    turn off warnings for deployment
    --HG--
    branch : hosted

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
  * This is an entry file for this Dataface Application.  To use your application
  * simply point your web browser to this file.
  */
+
+// Report simple running errors
+error_reporting(E_ERROR | E_PARSE);
+
 $time = microtime(true);
 	// use the timer to time how long it takes to generate a page
 require_once '/home/dckc/hh-office.dreamhosters.com/hh-office/lib/xataface-1.3rc6/dataface-public-api.php';
