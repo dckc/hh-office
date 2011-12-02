df9b1bf3bf9c4777f11400b4f1b53e8d23e07324
dconnolly@russ-macbook.local <dconnolly@russ-macbook.local>
2011-12-02 10:55:48 -0700

    toward claim sync: batch parsing works   - refactor _test_main() as class FreeClaimsUA
    --HG--
    branch : hosted

--- claim_sync.py
+++ claim_sync.py
@@ -1,59 +1,120 @@
-import mechanize
+'''claim_sync -- sync claims between hh-office and FreeClaims
+
+TODO: get batches of claims from hh-office
+'''
 import time
 import logging
+from collections import namedtuple
 
+from mechanize._beautifulsoup import BeautifulSoup as HTML
+import mechanize
 
 log = logging.getLogger(__name__)
 
 
-def _test_main(argv,
-    members_login_pg='https://sfreeclaims.anvicare.com/docs/member_login.asp'):
-    username, password, claim_fn = argv[1:4]
+class FreeClaimsUA(mechanize.Browser):
+    base = 'https://sfreeclaims.anvicare.com/docs/'
 
-    logging.basicConfig(level=logging.DEBUG)
-    
-    ua = mechanize.Browser()
+    def login(self, username, password, pg='member_login.asp'):
+        log.debug('login: open(%s)', self.base + pg)
+        self.open(self.base + pg)
+        self.select_form(name="loginForm")
+        self['username'] = username
+        self['userpassword'] = password
+        log.debug('login: submit creds.')
+        self.submit()
 
-    log.debug('1. open(%s)', members_login_pg)
-    ua.open(members_login_pg)
-    ua.select_form(name="loginForm")
-    ua['username'] = username
-    ua['userpassword'] = password
-    log.debug('2. submit creds.')
-    ua.submit()
+    def batches(self, pg='ViewBatch.asp'):
+        log.debug('batches: open(%s).', pg)
+        return self._batches(self.open(pg).get_data())
 
-    log.debug('2. open(%s) to get previous latest batch number.',
-              'ViewBatch.asp')
-    rprev = ua.open('ViewBatch.asp')
-    print '----------------ViewBatch.asp initial', '\n', rprev.get_data()
+    def _batches(self, txt):
+        doc = HTML(txt)
+        doc.done()
+        t = doc.html.first('table', {'align': 'Center'})
+        return [batch(row('td')[1])
+                # skip heading row
+                for row in t('tr')[1:]]
 
-    log.debug('3. open(upload.asp).')
-    r3 = ua.open('upload.asp')
-    print '----------------upload.asp', '\n', r3.get_data()
-    scrub_nested_form(ua, r3)
+    def upload_file(self, claim_fn, pg='upload.asp', form='Upload'):
+        log.debug('upload: open(%s).', pg)
+        r3 = self.open(pg)
+        scrub_nested_form(self, r3)
+        log.debug('upload: select_form(%s).', form)
+        self.select_form(form)
 
-    log.debug('3.a select_form(%s).', 'Upload')
-    ua.select_form('Upload')
+        log.debug('upload: add_file(%s).', claim_fn)
+        self.form.add_file(open(claim_fn), 'text/plain', claim_fn)
+        log.debug('upload: submit claims.')
+        r4 = self.submit()
+        # @@TODO: parse server-assigned name
+        return r4.get_data()
 
-    log.debug('3.b add_file(%s).', claim_fn)
-    ua.form.add_file(open(claim_fn), 'text/plain', claim_fn)
-    log.debug('4. submit claims.')
-    r4 = ua.submit()
-    print '----------------submit response', '\n', r4.get_data()
+    def upload_batch(self, claim_fn):
+        '''Upload a file of claims and wait for the new batch number.
 
-    while 1:
-        time.sleep(1)
-        log.debug('5.. open(%s)', 'ViewBatch.asp')
-        rbatches = ua.open('ViewBatch.asp')
-        content = rbatches.get_data()
-        print '----------------ViewBatch.asp', '\n', content
-        break  #@@
+        @return: list of batches for this account, starting with the new one.
+
+        We assume noone else is uploading concurrently.
+        '''
+        before = self.batches()
+        self.upload_file(claim_fn)
+        while 1:
+            log.debug('wait: sleep 2')
+            time.sleep(2)
+            b = self.batches()
+            if b[0].batch_no > before[0].batch_no:
+                return b
+
+
+Batch = namedtuple('Batch', 'batch_no href')
+
+
+def batch(cell):
+    '''
+    >>> batch(HTML('<td nowrap="nowrap">&nbsp;'
+    ...              '<a href="ViewClaims.asp?batch_no=2027161">2027161</a>'
+    ...             '</td>'))
+    Batch(batch_no=2027161, href='ViewClaims.asp?batch_no=2027161')
+    '''
+    return Batch(int(cell.a.contents[0]), cell.a['href'])
+
+
+def _test_main(argv):
+    import sys
+    import pprint
+
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
+    ua = FreeClaimsUA()
+
+    while argv[1:]:
+        action = argv[1]
+        if action == '--login':
+            username, password = argv[2:4]
+            del argv[1:4]
+            ua.login(username, password)
+        elif action == '--upload':
+            claim_fn = argv[2]
+            del argv[1:3]
+            ua.upload(claim_fn)
+        elif action == '--batches':
+            del argv[1]
+            pprint.pprint(ua.batches())
+        elif action == '--batches-local':
+            doc_fn = argv[2]
+            del argv[1:3]
+            pprint.pprint(ua._batches(open(doc_fn).read()))
+        else:
+            raise ValueError('huh? ' + action)
 
 
 def scrub_nested_form(ua, response):
-    # http://stackoverflow.com/questions/1782368/is-it-possible-to-hook-up-a-more-robust-html-parser-to-python-mechanize/5039584#5039584
-    # http://stackoverflow.com/questions/7135964/submitting-nested-form-with-python-mechanize
-    tweak = '''<form method="POST" action="FRCLsaveClaim" enctype="multipart/form-data" name="uploadClaimForm">'''
+    # Is it possible to hook up a more robust HTML parser to Python mechanize?
+    # http://stackoverflow.com/questions/1782368/
+    #  submitting-nested-form-with-python-mechanize
+    # http://stackoverflow.com/questions/7135964/
+    tweak = ('''<form method="POST" action="FRCLsaveClaim" '''
+             '''enctype="multipart/form-data" name="uploadClaimForm">''')
     log.debug('tweak code')
     txt = response.get_data()
     if tweak in txt:
@@ -63,31 +124,6 @@ def scrub_nested_form(ua, response):
         ua.set_response(response)
 
 
-def _test_parse(argv):
-    x, y, doc_fn = argv[1:4]
-    import ClientForm
-    forms = ClientForm.ParseFile(open(doc_fn), 'http://example/')
-    print forms
-
-
-def _test_soup(argv):
-    doc_fn = argv[1]
-    from mechanize._beautifulsoup import BeautifulSoup as Soup
-    doc = Soup(text=open(doc_fn).read())
-    doc.done()
-    print doc.html.name
-    #print [tag.name for tag in doc.html.fetch()]
-    t = doc.html.first('table', 'sortable')  #, recursive=1
-    #print t
-    #print t.prettify()
-    import pprint
-    print [tag.name for tag in t.fetch('tr', recursive=False)]
-    pprint.pprint([(row.name, [cell.name for cell in row('td') + row('th')])
-                   for row in t('tr')])
-
-
 if __name__ == '__main__':
     import sys
-    _test_soup(sys.argv)
-    #_test_parse(sys.argv)
-    #_test_main(sys.argv)
+    _test_main(sys.argv)
