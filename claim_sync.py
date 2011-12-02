ec5f3ad27a8760e6e29797805a6ba2eb739297d5
dconnolly@russ-macbook.local <dconnolly@russ-macbook.local>
2011-12-02 12:25:29 -0700

    toward claim sync: scrape table of claims to get trace_no, href, etc.
    --HG--
    branch : hosted

--- claim_sync.py
+++ claim_sync.py
@@ -5,6 +5,7 @@ TODO: get batches of claims from hh-office
 import time
 import logging
 from collections import namedtuple
+import datetime
 
 from mechanize._beautifulsoup import BeautifulSoup as HTML
 import mechanize
@@ -35,6 +36,13 @@ class FreeClaimsUA(mechanize.Browser):
         return [batch(row('td')[1])
                 # skip heading row
                 for row in t('tr')[1:]]
+
+    def claims(self, batch):
+        log.debug('claims: open (%s)', batch.href)
+        doc = HTML(self.open(batch.href).get_data())
+        doc.done()
+        t = doc.html.first('table', {'align': 'Center'})
+        return [claim(row) for row in t('tr')[1:]]
 
     def upload_file(self, claim_fn, pg='upload.asp', form='Upload'):
         log.debug('upload: open(%s).', pg)
@@ -80,6 +88,51 @@ def batch(cell):
     return Batch(int(cell.a.contents[0]), cell.a['href'])
 
 
+Claim = namedtuple('Claim',
+                   'trace_no href batch status last first service_date')
+
+
+def claim(row):
+    '''
+    >>> claim(HTML(_CLAIM_MARKUP))
+    ... # doctest: +NORMALIZE_WHITESPACE
+    Claim(trace_no=33298999, href='viewonehcfa.asp?trace_no=33298999',
+          batch=2027161, status='ACCEPTED', last='SMITH', first='STEVEN',
+          service_date=datetime.datetime(2011, 11, 10, 0, 0))
+
+    '''
+    txts = [cell.contents[0].replace('&nbsp;', '') for cell in row('td')]
+    trace_cell = row('td')[1]
+    return Claim(int(trace_cell.a.contents[0]), trace_cell.a['href'],
+                 int(txts[2]), txts[3], txts[4], txts[5],
+        datetime.datetime.strptime(txts[9], '%m/%d/%Y'))
+
+_CLAIM_MARKUP = '''
+                            <tr bgcolor=ddf7f7 >
+                                <td class="g" align="right">1</td>
+
+
+                <td class="g"><a href='viewonehcfa.asp?trace_no=33298999'
+                target='_new'>33298999</a></td>
+
+
+                                <td class="g">2027161</td>
+                                <td class="g">ACCEPTED</td>
+                                <td class="g">&nbsp;SMITH</td>
+                                <td class="g">&nbsp;STEVEN</td>
+                                <td class="g">&nbsp;JAM.STE.10999</td>
+                                <td class="g">&nbsp;60999</td>
+                                <td class="g">&nbsp;MDONL</td>
+                                <td class="g">&nbsp;11/10/2011</td>
+                                <td class="g" align="right">&nbsp;120.00</td>
+                                <td class="g">&nbsp;</td>
+                                <td class="g">&nbsp;</td>
+                                <td class="g">&nbsp;JOHNSON</td>
+                                <td class="g">&nbsp;WILLIAM</td>
+                         <td class="g" align="right">&nbsp;12/2/2011</td>
+                            </tr>
+'''
+
 def _test_main(argv):
     import sys
     import pprint
@@ -104,6 +157,10 @@ def _test_main(argv):
             doc_fn = argv[2]
             del argv[1:3]
             pprint.pprint(ua._batches(open(doc_fn).read()))
+        elif action == '--claims':
+            del argv[1]
+            pprint.pprint([(batch, ua.claims(batch))
+                            for batch in ua.batches()])
         else:
             raise ValueError('huh? ' + action)
 
