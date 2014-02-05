0b69c6927cfbcf800f5c8c3040e32f3008dd6a5b
Dan Connolly <http://www.madmode.com/>
2014-02-05 17:41:36 -0600

    adapted report_gen to least-authority style
    --HG--
    branch : no-rlib

--- report_gen.py
+++ report_gen.py
@@ -1,10 +1,22 @@
-# Minimal Example
+# cribbed Minimal Example
 # from
 # https://code.google.com/p/pyfpdf/wiki/Tutorial
+#
+# adapted to least-authority style
+# http://www.madmode.com/2013/python-capability-idioms-part-1.html
 from fpdf import FPDF
 
-pdf=FPDF()
-pdf.add_page()
-pdf.set_font('Arial','B',16)
-pdf.cell(40,10,'Hello World!')
-pdf.output('tuto1.pdf','F')
+
+def main(save):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.cell(40, 10, 'Hello World!')
+    save(pdf.output('', 'S'))
+
+
+if __name__ == '__main__':
+    def _with_caps(out='tuto1.pdf'):
+        main(save=lambda x: open(out, 'w').write(x))
+
+    _with_caps()
