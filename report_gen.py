fae8670ede1cb567834557dab97e095e1dde09c7
Dan Connolly <http://www.madmode.com/>
2014-02-06 20:33:25 -0600

    - detail lines based on input  - a bit more typography: normal_line_height to go with Courier
    --HG--
    branch : no-rlib

--- report_gen.py
+++ report_gen.py
@@ -12,15 +12,14 @@ __ https://code.google.com/p/pyfpdf/wiki/Tutorial
 from fpdf import FPDF
 
 
-def main(stdout,
-         report_header='Reduced Fees'):
+def main(stdin, stdout):
     '''
     Note `_with_caps()` below for `least-authority style`__.
 
     __ http://www.madmode.com/2013/python-capability-idioms-part-1.html
     '''
     rpt = OfficeReport()
-    rpt.run(report_header)
+    rpt.run(report_header=stdin.next(), lines=stdin)
     stdout.write(rpt.pdf_string())
     raise NotImplementedError('database input, detail, breaks, etc.')
 
@@ -30,16 +29,28 @@ class OfficeReport(FPDF):
     full_line = 0
     then_right, then_newline, then_below = 0, 1, 2
     align_left, align_center, align_right = 'L', 'C', 'R'
+    normal_line_height = 1.2
 
     def __init__(self,
-                 orientation='P', units='pt', format='Letter'):
+                 orientation='P', units='pt', format='Letter',
+                 detail_size=10):
         FPDF.__init__(self, orientation, units, format)
+        self.font_size = 10
 
     def run(self, report_header,
+            lines,
             large=12, bold='B'):
         self.add_page()
         self.set_font(self.font, bold, large)
-        self.cell(self.full_line, txt=report_header, ln=self.then_newline)
+        self.cell(self.full_line, h=large * self.normal_line_height,
+                  txt=report_header, ln=self.then_newline)
+        self.detail(lines)
+
+    def detail(self, lines):
+        self.set_font(self.font, '', self.font_size)
+        for line in lines:
+            self.cell(self.full_line, self.font_size * self.normal_line_height,
+                      txt=line, ln=self.then_newline)
 
     def header(self):
         pass
@@ -59,7 +70,7 @@ class OfficeReport(FPDF):
 
 if __name__ == '__main__':
     def _with_caps(out='tuto1.pdf'):
-        from sys import stdout
-        main(stdout=stdout)
+        from sys import stdin, stdout
+        main(stdin, stdout)
 
     _with_caps()
