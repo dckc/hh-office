c8a72eb6fc745f1abd947d9ffb7c4f04f1c05dc8
Dan Connolly <http://www.madmode.com/>
2014-02-06 21:11:41 -0600

    parse report design document and use it to determine header
    --HG--
    branch : no-rlib

--- report_gen.py
+++ report_gen.py
@@ -9,17 +9,20 @@ __ https://code.google.com/p/pyfpdf/wiki/Tutorial
 
 '''
 
+from xml.etree import ElementTree as ET
+
 from fpdf import FPDF
 
 
-def main(stdin, stdout):
+def main(argv, stdin, stdout, open_arg):
     '''
     Note `_with_caps()` below for `least-authority style`__.
 
     __ http://www.madmode.com/2013/python-capability-idioms-part-1.html
     '''
-    rpt = OfficeReport()
-    rpt.run(report_header=stdin.next(), lines=stdin)
+    design_doc = argv[1]
+    rpt = OfficeReport.make(open_arg(design_doc))
+    rpt.run(lines=stdin)
     stdout.write(rpt.pdf_string())
     raise NotImplementedError('database input, detail, breaks, etc.')
 
@@ -31,19 +34,26 @@ class OfficeReport(FPDF):
     align_left, align_center, align_right = 'L', 'C', 'R'
     normal_line_height = 1.2
 
-    def __init__(self,
+    def __init__(self, design,
                  orientation='P', units='pt', format='Letter',
                  detail_size=10):
         FPDF.__init__(self, orientation, units, format)
         self.font_size = 10
+        self.design = design
 
-    def run(self, report_header,
-            lines,
+    @classmethod
+    def make(cls, fp):
+        return cls(ET.parse(fp))
+
+    def run(self, lines,
             large=12, bold='B'):
         self.add_page()
+
+        hd_txt = HTML.by_class(self.design, 'h1', 'ReportHeader')[0].text
         self.set_font(self.font, bold, large)
         self.cell(self.full_line, h=large * self.normal_line_height,
-                  txt=report_header, ln=self.then_newline)
+                  txt=hd_txt, ln=self.then_newline)
+
         self.detail(lines)
 
     def detail(self, lines):
@@ -68,9 +78,30 @@ class OfficeReport(FPDF):
         return cls.todo() if classname else default
 
 
+class HTML(object):
+    r'''
+    >>> ctx = ET.fromstring('<body><h1 class="x y">..</h1></body>')
+    >>> [(e.tag, e.attrib['class']) for e in HTML.by_class(ctx, 'h1', 'x')]
+    [('h1', 'x y')]
+    '''
+    @classmethod
+    def by_class(cls, ctx, tagname, classname):
+        w_class = ctx.findall(
+            ".//h:%s[@class]" % tagname,
+            namespaces=dict(h='http://www.w3.org/1999/xhtml'))
+        return [e for e in w_class
+                if classname in e.attrib['class'].split()]
+
+
 if __name__ == '__main__':
     def _with_caps(out='tuto1.pdf'):
-        from sys import stdin, stdout
-        main(stdin, stdout)
+        from sys import argv, stdin, stdout
+
+        def open_arg(n):
+            if n not in argv:
+                raise IOError
+            return open(n)
+
+        main(argv[:], stdin, stdout, open_arg)
 
     _with_caps()
