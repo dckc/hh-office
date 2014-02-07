490637d0f6b04764e3c085027b1d6f9e9f6d31e3
Dan Connolly <http://www.madmode.com/>
2014-02-06 21:19:20 -0600

    determine orientation from design doc
    --HG--
    branch : no-rlib

--- report_gen.py
+++ report_gen.py
@@ -28,6 +28,7 @@ def main(argv, stdin, stdout, open_arg):
 
 
 class OfficeReport(FPDF):
+    portrait, landscape = 'P', 'L'
     font = 'Courier'
     full_line = 0
     then_right, then_newline, then_below = 0, 1, 2
@@ -35,9 +36,9 @@ class OfficeReport(FPDF):
     normal_line_height = 1.2
 
     def __init__(self, design,
-                 orientation='P', units='pt', format='Letter',
+                 unit='pt', format='Letter',
                  detail_size=10):
-        FPDF.__init__(self, orientation, units, format)
+        FPDF.__init__(self, unit=unit, format=format)
         self.font_size = 10
         self.design = design
 
@@ -45,16 +46,22 @@ class OfficeReport(FPDF):
     def make(cls, fp):
         return cls(ET.parse(fp))
 
-    def run(self, lines,
-            large=12, bold='B'):
-        self.add_page()
+    def run(self, lines):
+        self.start_page()
+        self.detail(lines)
+
+    def start_page(self,
+                   large=12, bold='B'):
+        self.add_page(
+            orientation=(
+                self.landscape
+                if HTML.by_class(self.design, 'body', 'landscape')
+                else self.portrait))
 
         hd_txt = HTML.by_class(self.design, 'h1', 'ReportHeader')[0].text
         self.set_font(self.font, bold, large)
         self.cell(self.full_line, h=large * self.normal_line_height,
                   txt=hd_txt, ln=self.then_newline)
-
-        self.detail(lines)
 
     def detail(self, lines):
         self.set_font(self.font, '', self.font_size)
