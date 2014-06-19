7cbd2c703c153f7994639d05549fdc6662866917
Dan Connolly <http://www.madmode.com/>
2014-06-18 20:01:21 -0500

    first draft of Income by Therapist report
    --HG--
    branch : etree26

--- tables/Therapist/Therapist.php
+++ tables/Therapist/Therapist.php
@@ -1,3 +1,11 @@
 <?
 class tables_Therapist extends Audited {
+  function block__tables_menu_head () {
+    $app =& Dataface_Application::getInstance();
+    $key = $app->_conf['_database']['report_key'];
+    echo "<ul class='report_menu'>
+           <li><a href='print_report/income_by_therapist?key=$key'
+	          target='_new'>Income By Therapist</a></li>
+          </ul>";
+  }
 }
